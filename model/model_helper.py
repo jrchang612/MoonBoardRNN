9e83c71bff68ca03a7cbde7c22946cc55359d808
jrchang612 <jrchang612@gmail.com>
2020-06-06 15:55:57 -0700

    modify code

--- model/model_helper.py
+++ model/model_helper.py
@@ -7,6 +7,10 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import seaborn as sn
 import pandas as pd
+import matplotlib.image as mpimg
+import matplotlib.cbook as cbook
+import re 
+import PIL
 
 def save_pickle(data, file_name):
     """
@@ -45,10 +49,16 @@ def plot_history(history_all, model_name):
     loss = []
     val_loss = []
     for history in history_all:
-        acc.append(history.history['sparse_categorical_accuracy'])
-        val_acc.append(history.history['val_sparse_categorical_accuracy'])
-        loss.append(history.history['loss'])
-        val_loss.append(history.history['val_loss'])
+        if 'sparse_categorical_accuracy' in history.history.keys():
+            acc.append(history.history['sparse_categorical_accuracy'])
+            val_acc.append(history.history['val_sparse_categorical_accuracy'])
+            loss.append(history.history['loss'])
+            val_loss.append(history.history['val_loss'])
+        else:
+            acc.append(history.history['accuracy'])
+            val_acc.append(history.history['val_accuracy'])
+            loss.append(history.history['loss'])
+            val_loss.append(history.history['val_loss'])
 
     acc = sum(acc, [])
     val_acc = sum(val_acc, [])
@@ -75,6 +85,39 @@ def plot_history(history_all, model_name):
                        'val_acc': val_acc, 
                        'loss': loss, 
                        'val_loss': val_loss}
+    return history_package
+
+
+def plot_history_no_val(history_all, model_name):
+    """
+    Plot the training history of the model, for the ones without validation set
+    """
+    # Plot training & validation accuracy values
+    acc = []
+    loss = []
+    for history in history_all:
+        acc.append(history.history['accuracy'])
+        loss.append(history.history['loss'])
+
+    acc = sum(acc, [])
+    loss = sum(loss, [])
+
+    fig, axes = plt.subplots(nrows = 1, ncols = 2, dpi = 150)
+    axes[0].plot(acc)
+    axes[0].set_title('Accuracy of '+ model_name)
+    axes[0].set_ylabel('Accuracy')
+    axes[0].set_xlabel('Epoch')
+    axes[0].legend(['Train'], loc='upper left')
+
+    axes[1].plot(loss)
+    axes[1].set_title('Loss of ' + model_name)
+    axes[1].set_ylabel('Loss')
+    axes[1].set_xlabel('Epoch')
+    axes[1].legend(['Train'], loc='upper left')
+    plt.tight_layout()
+    
+    history_package = {'acc': acc, 
+                       'loss': loss}
     return history_package
 
 def plot_history_package(history_package, model_name):
@@ -114,3 +157,101 @@ def compute_accuracy(y_true, y_predict):
     complete_accurate = np.sum(y_true == y_predict)/len(y_true)
     roughly_accurate = np.sum(np.abs(y_true - y_predict) <= 1)/len(y_true)
     return (complete_accurate, roughly_accurate)
+
+
+""" Draw a moonboard problem on the layout"""
+def plotAProblem(stringList, title = None, key = None):
+    cwd = os.getcwd()
+    parent_wd = cwd.replace('/model', '')
+    image_file = cbook.get_sample_data(parent_wd + "/raw_data/moonboard2016Background.jpg")
+    img = plt.imread(image_file)
+    x = []
+    y = []
+    for hold in stringList:
+        # Using re.findall() 
+        # Splitting text and number in string  
+        res = [re.findall(r'(\w+?)(\d+)', hold.split("-")[0])[0]] 
+        
+        alphabateList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"] 
+        ixInXAxis = alphabateList.index(res[0][0]) 
+     
+        x = x + [(90 + 52 * ixInXAxis)]# * img.shape[0] / 1024]
+        y = y + [(1020 - 52 * int(res[0][1]))]# * img.shape[1] / 1024]
+
+    # Create a figure. Equal aspect so circles look circular
+    plt.rcParams["figure.figsize"] = (10,20)
+    fig,ax = plt.subplots(1)
+    ax.set_aspect('equal')
+
+    # Show the image
+    ax.imshow(img)
+
+    # Now, loop through coord arrays, and create a circle at each x,y pair
+    count = 0
+    for xx,yy in zip(x,y):
+        if yy == 84:
+            circ = plt.Circle((xx,yy), 30, color = 'r', fill=False, linewidth = 2)
+        elif count < 2:
+            circ = plt.Circle((xx,yy), 30, color = 'g', fill=False, linewidth = 2)
+        else:
+            circ = plt.Circle((xx,yy), 30, color = 'b', fill=False, linewidth = 2)
+        ax.add_patch(circ)
+        count = count + 1
+
+    # Show the image
+    if title:
+        plt.title(title)
+        plt.savefig(key + '.jpg', dpi = 200)
+    plt.show()
+
+def convert_num_to_V_grade(num):
+    dic = {0: 'V4', 1: 'V5', 2: 'V6', 3:'V7', 4:'V8', 5: 'V9', 6:'V10', 7: 'V11', 8:'V12', 9:'V13'}
+    return dic[num]
+
+def normalization(input_set):
+    mu_x = 5.0428571
+    sig_x = 3.079590
+    mu_y = 9.8428571
+    sig_y = 4.078289957
+    mu_hand = 4.2428571
+    sig_hand = 2.115829552
+    mu_diff = 12.118308
+    sig_diff = 11.495348196
+    
+    mu_vec = np.array([mu_x, mu_y, 0, mu_hand, mu_x, mu_y, mu_hand, 0, 0, mu_x, mu_y, mu_hand, 0, 0, 0, 0, 0, 0, 0, 0, 0, mu_diff])
+    sig_vec = np.array([sig_x, sig_y, 1, sig_hand, sig_x, sig_y, sig_hand, sig_x, sig_y, sig_x, sig_y, sig_hand, sig_x, sig_y, 1, 1, 1, 1, 1, 1, 1, sig_diff])
+    
+    mask = np.zeros_like(input_set['X'])
+    for i in range(len(mask)):
+        mask[i, 0:int(input_set['tmax'][i]), :] = 1
+    
+    X_normalized = np.copy(input_set['X'])
+    X_normalized -= mu_vec
+    X_normalized /= sig_vec
+    X_normalized *= mask
+    
+    output_set = input_set
+    output_set['X'] = X_normalized
+    return output_set
+
+def convert_generated_data_into_test_set(generated_problems, save_path):
+    n_sample = len(generated_problems)
+    X_seq_data_merge = np.zeros((n_sample, 12, 22))
+    keys_seq_merge = []
+    tmax_seq_merge = np.zeros(n_sample)
+
+    i = 0
+    for key, value in generated_problems.items():
+        X_data = value.T
+        X_seq_data_merge[i, 0:X_data.shape[0], :] = X_data
+        keys_seq_merge.append(key)
+        tmax_seq_merge[i] = X_data.shape[0]
+        i = i + 1
+    
+    test_set_gen = {'X': X_seq_data_merge,   
+                'keys': keys_seq_merge, 
+                'tmax': tmax_seq_merge}
+    
+    test_set_gen_normalized = normalization(test_set_gen)
+    save_pickle(test_set_gen_normalized, save_path)
+    return test_set_gen_normalized
--- preprocessing/preprocessing_helper.py
+++ preprocessing/preprocessing_helper.py
@@ -797,7 +797,7 @@ def produce_sequence(keyNum, X_dict, n_return = 1, printout = False):
       status.handSequence, status.handOperator, status.overallSuccessRate(), status.successScoreSequence
     '''
     moonboardTest = X_dict[keyNum]
-    moonboardTestUrl = MoonBoard_2016_withurl[keyNum]
+    #moonboardTestUrl = MoonBoard_2016_withurl[keyNum]
     testbeta = beta(moonboardTest.T)
     status = [beta(moonboardTest.T), beta(moonboardTest.T)]
     status[0].addStartHolds(0)
@@ -826,7 +826,7 @@ def produce_sequence(keyNum, X_dict, n_return = 1, printout = False):
     # produce output
     output = {}
     if printout == True:
-        print(moonboardTestUrl["url"])
+        #print(moonboardTestUrl["url"])
         print ("After Beamer search, the most possible hand sequence and the successRate:")
     for i in largestIndex:
         output[i] = status[i]
@@ -841,6 +841,49 @@ def coordinateToString(coordinate):
     """ convert (9.0 ,4.0) to "J5" """
     alphabateList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
     return str(alphabateList[int(coordinate[0])]) + str(int(coordinate[1]) + 1)
+
+# =============================================================
+# Pure hand sequence generation
+# =============================================================
+
+def handStringSeqGeneratorForAllProblem(processed_data, save_path, print_result = False):
+    """
+    Apply moveGenerator on all problems, and transform the output into X vector move sequence for RNN
+    Input:
+    - processed_data: the processed data that is scraped from MoonBoard
+    - i.e. '/preprocessing/processed_data_xy_mode'  MoonBoard_2016_raw["X_dict_benchmark_withgrade"]
+    
+    Output:
+    dictionary-- key: handSeqList
+    i.e.
+    ["J5-LH", "G6-RH" ...."D18-RH"]. This sequence will be feed into RNN as hand seq corps to train route generator
+    """
+    output = {}
+    list_fail = []
+    
+    for key in processed_data.keys():
+        # create x_vector
+        try:
+            beamerBeta = produce_sequence(key, processed_data, n_return = 1)[0]
+            numOfMoves = len(beamerBeta.handSequence) 
+            routeHandSequence = beamerBeta.handSequence    
+            routeOpSequence = beamerBeta.handOperator 
+            handStringList = []
+            
+            for orderOfHand in range(numOfMoves):  
+                targetCoordinate = beamerBeta.getXYFromOrder(routeHandSequence[orderOfHand])
+                newHandStr = coordinateToString(targetCoordinate) + "-" + routeOpSequence[orderOfHand]
+                handStringList.append(newHandStr)
+            if print_result:
+                print('Complete %s' %key)
+            output[key] = handStringList  
+        except:
+            print('Raw data with key %s contains error' %key)
+            list_fail.append(key)
+
+    save_pickle(output, save_path)
+    print('result saved.')
+    return output
 
 # =============================================================
 # Normalization
@@ -891,3 +934,125 @@ def normalization_move(input_set):
     output_set = input_set
     output_set['X'] = X_normalized
     return output_set
+
+def moveGenerator(beta, string_mode = True):
+    """ generate the final output of move sequence as a list of dictionary.
+    Length of the list: how many moves in this climb to the target hold. Target holds run from the third order hold to the last hold
+    Dictionary involves all information needed to evaluate grade/ analyze style for human. This is a basic building block of the route.
+    TargetHoldString : "A10" for example
+    TargetHoldHand: "RH" for example 
+    TargetHoldScore: the difficulty to hold on the target hold applying the "RH" operation
+    RemainingHoldString : "A10" for example
+    RemainingHoldHand: 
+    RemainingHoldScore 
+    MovingHoldString : A10 for example
+    MovingHoldHand: 
+    MovingHoldScore: 
+    dxdyMtoT: vector Target - moving hand. This distance's physical meaning is the real hand traveling range during the move
+    dxdyRtoT: vector Target - Remaining hand. This distance's physical meaning is the inter distance between two remaining hand after finish the move
+    FootPlacement: [0,0,0,0,1,1,0] means there is hold on region 5 and 6. 
+    MoveSuccessRate: estimation of how easy of this move 
+    if coordinate_mode = True, String will be coordinate form and 
+    """
+    
+    outputDictionaryList = []
+    numOfMoves = len(beta.handSequence) - 2 # calculate from the third hold to the end hold (no final match)
+    # loop over holds from third one to the finish hold (rank from 3 to end). In each move, this is the hold defined as target hold
+    for rank in range(2,  len(beta.handSequence)):
+        # Renew a dictionary
+        moveDictionary = {}
+        
+        # Define target hold
+        order = beta.handSequence[rank] # order is the original order (int) of hold read from bottom to top
+        targetHoldHand = beta.handOperator[rank]
+        coordinateOfTarget = beta.getXYFromOrder(order) 
+        
+        if string_mode == False:
+            moveDictionary["TargetHoldString"] = coordinateOfTarget
+            if targetHoldHand == "LH":
+                moveDictionary["TargetHoldHand"] = 0   # LH ->0
+            else: moveDictionary["TargetHoldHand"] = 1  # RH -> 1 
+        else: 
+            moveDictionary["TargetHoldString"] = coordinateToString(coordinateOfTarget)
+            moveDictionary["TargetHoldHand"] = targetHoldHand
+        moveDictionary["TargetHoldScore"] = holdScoreUseCordAndOp(coordinateOfTarget, targetHoldHand)   # Could you file I/O excile file L/R hand difficulty?
+        
+        # Define remaining hold
+        listBeforeTargetHold = beta.handOperator[0:rank]
+
+        remainingHoldHand = oppositehand(targetHoldHand)
+        order = int(''.join(listBeforeTargetHold).rindex(remainingHoldHand)/2) # remaining hold is the last hold with opposite hand in the sequence before Target hand
+        coordinateOfRemaining = beta.getXYFromOrder(beta.handSequence[order]) 
+    
+        if string_mode == False:
+            moveDictionary["RemainingHoldString"] = coordinateOfRemaining
+            moveDictionary["RemainingHoldHand"] = 1 - moveDictionary["TargetHoldHand"]
+        else: 
+            moveDictionary["RemainingHoldString"] = coordinateToString(coordinateOfRemaining)
+            moveDictionary["RemainingHoldHand"] = remainingHoldHand
+        moveDictionary["RemainingHoldScore"] = holdScoreUseCordAndOp(coordinateOfRemaining, remainingHoldHand)
+        moveDictionary["dxdyRtoT"] = (coordinateOfTarget[0] - coordinateOfRemaining[0], coordinateOfTarget[1] - coordinateOfRemaining[1])
+        
+        # Define moving hold
+        movingHoldHand = targetHoldHand
+        order = int(''.join(listBeforeTargetHold).rindex(movingHoldHand)/2) # remaining hold is the last hold with opposite hand in the sequence before Target hand
+        coordinateOfMoving = beta.getXYFromOrder(beta.handSequence[order]) 
+        
+        if string_mode == False:
+            moveDictionary["MovingHoldString"] = coordinateOfMoving
+            moveDictionary["MovingHoldHand"] = moveDictionary["TargetHoldHand"]
+        else: 
+            moveDictionary["MovingHoldString"] = coordinateToString(coordinateOfMoving)
+            moveDictionary["MovingHoldHand"] = movingHoldHand
+        moveDictionary["MovingHoldScore"] = holdScoreUseCordAndOp(coordinateOfMoving, movingHoldHand)
+        moveDictionary["dxdyMtoT"] = (coordinateOfTarget[0] - coordinateOfMoving[0], coordinateOfTarget[1] - coordinateOfMoving[1])
+        
+        # Define foot region location
+        x0, y0 = int(coordinateOfRemaining[0]), int(coordinateOfRemaining[1])
+        region0 = [(x,y) for x in range(x0 - 4, x0 - 1) for y in range(y0 - 3, y0 - 1)]
+        region1 = [(x,y) for x in range(x0 - 1, x0 + 2) for y in range(y0 - 3, y0 - 1)]
+        region2 = [(x,y) for x in range(x0 + 2, x0 + 5) for y in range(y0 - 3, y0 - 1)]
+        region3 = [(x,y) for x in range(x0 - 5, x0 - 1) for y in range(y0 - 6, y0 - 3)]
+        region4 = [(x,y) for x in range(x0 - 1, x0 + 2) for y in range(y0 - 6, y0 - 3)]
+        region5 = [(x,y) for x in range(x0 + 2, x0 + 6) for y in range(y0 - 6, y0 - 3)]
+        region6 = [(x,y) for x in range(x0 - 2, x0 + 3) for y in range(y0 - 9, y0 - 6)]
+
+        # check is there foot holds in the region
+        footholdList = [0] * 7 
+        regionList = [region0, region1, region2, region3, region4, region5, region6]
+        for hold in beta.allHolds:
+            holdx, holdy = hold[6], hold[7]
+            for i in range(7):
+                if (holdx, holdy) in regionList[i]:
+                    footholdList[i] = 1 
+            # deal with additional footholds        
+            if region1[0][1] < 0: # if the lowest hold in region1 is < 0, we can use additional footholds (region's first element start from the lowest)
+                footholdList[0] = 1
+                footholdList[1] = 1
+                footholdList[2] = 1
+            elif region4[0][1] < 0: # if the lowest hold in region1 is < 0, we can use additional footholds
+                footholdList[3] = 1
+                footholdList[4] = 1
+                footholdList[5] = 1 
+            elif region6[0][1] < 0: # if the lowest hold in region1 is < 0, we can use additional footholds
+                footholdList[6] = 1
+            
+        moveDictionary["FootPlacement"] = footholdList  
+        
+        # Add the singlemoveSuccessRate
+        if coordinateOfMoving == coordinateOfRemaining:  ## If start from the match position
+            pass # May need special consideration when match hand
+        if targetHoldHand == "RH": 
+            scoreFromDistance =  makeGaussian(coordinateOfTarget, 3, coordinateOfRemaining, "LH")
+        if targetHoldHand == "LH": 
+            scoreFromDistance =  makeGaussian(coordinateOfTarget, 3, coordinateOfRemaining, "RH")
+            
+        scoreFromfoot = 1    
+        if sum(footholdList) < 1: scoreFromfoot = 0.5  
+ 
+        moveSuccessRate = moveDictionary["RemainingHoldScore"] * moveDictionary["TargetHoldScore"] * scoreFromDistance * scoreFromfoot
+        moveDictionary["MoveSuccessRate"] = moveSuccessRate
+        
+        # Finish fill in all components of a move
+        outputDictionaryList.append(moveDictionary)
+    return outputDictionaryList
