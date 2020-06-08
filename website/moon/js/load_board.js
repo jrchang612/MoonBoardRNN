896d286f4b36da89b1b935647dd111b9568b14f9
jrchang612 <jrchang612@gmail.com>
2020-06-08 16:41:21 -0700

    modify image

--- website/moon/js/load_board.js
+++ website/moon/js/load_board.js
@@ -25,8 +25,10 @@ function clear_board() {
 function random_climb() {
     var model = document.querySelector('input[name="model"]:checked').value;
     var index = Math.floor(Math.random() * climbs[model].length)
+    var background = document.getElementsByClassName("background-image");
     display(climbs[model][index].moves)
     display_grades(climbs[model][index].grade)
+    background.style.backgroundImage = "url("+climbs[model][index].file_name+")";
 }
 
 function display(moves) {
